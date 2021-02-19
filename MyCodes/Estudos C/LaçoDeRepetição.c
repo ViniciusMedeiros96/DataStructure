c5dc0806cb5439e7bb977a5430c0bdbe80453282
vinimedeiros <vinimedeiros7@outlook.com>
2021-02-18 21:22:37 -0300

    atualizando

--- MyCodes/Estudos C/LaçoDeRepetição.c
+++ MyCodes/Estudos C/LaçoDeRepetição.c
@@ -7,7 +7,7 @@ int main(){
     for (contador = 1; contador <= 10; contador += 1){
         printf("teste\n");
     }
-    printf("---------------");
+    printf("------------");
     while (contador2 <= 5){
         printf("%i\n",contador2);
         ++contador2;
