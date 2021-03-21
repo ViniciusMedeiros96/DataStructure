45714ec8699b36511f740d97563c1d0d407e06a6
vinimedeiros <vinimedeiros7@outlook.com>
2021-03-21 20:15:22 -0300

    Adicionando estudos da aula

--- MyCodes/Estudos C/Apontadores/TestPonteiros.c
+++ MyCodes/Estudos C/Apontadores/TestPonteiros.c
@@ -5,7 +5,7 @@ int main (void){
     char vetor [] = "Cesar";
     char *pv = vetor;
     pv[2] = 'Z';
-    *(pv+1) = 'RR';
+    *(pv+1) = 'R';
     printf("%c\n", *(pv+2));
     printf("%c\n", pv[1]);
     
